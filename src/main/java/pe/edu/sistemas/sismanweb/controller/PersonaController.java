d0b1b8b26f15da7e7ce6f3bedd0d00ae674f2e63
andISoft <andcristhian95@gmail.com>
2017-06-01 03:53:23 -0500

    Servlet de SpringConfig 50%

--- src/main/java/pe/edu/sistemas/sismanweb/controller/PersonaController.java
+++ src/main/java/pe/edu/sistemas/sismanweb/controller/PersonaController.java
@@ -1,17 +1,28 @@
 package pe.edu.sistemas.sismanweb.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/example")
 public class PersonaController {
 	
-	@GetMapping("/prueba")
-	public String exampleString(){
-		return "example";
-	}
+	protected final Log logger = LogFactory.getLog(getClass());
+	
+	@RequestMapping(value="/exam.htm")
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        logger.info("Returning hello view");
+        return new ModelAndView("example.html");
+    }
 	
 
 }
